dfc76af394cbef2daf807a0a6f6c47d16d279218
Vaibhav Matere <vpm00718897@gmail.com>
2023-11-29 19:55:50 +0530

    socket func simultaneous draw

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require('cors');
+const { emit } = require("process");
 
 const app = express();
 
@@ -16,4 +17,18 @@ io.on("connection", (socket) => {
      console.log('server is connected');
 });
 
-httpServer.listen(8000);
+// when draw - it should reflect to all other screens
+socket.on('beginPath', (arg) => {
+     socket.broadcast.emit('beginPath', arg)
+});
+
+// to change color and size in other windows simultaneously
+socket.on('drawLine', (arg) => {
+     socket.broadcast.emit('drawLine', arg)
+});
+
+socket.on('changeConfigs', (arg) => {
+     socket.broadcast.emit('changeConfigs', arg)
+});
+
+httpServer.listen(5000);
